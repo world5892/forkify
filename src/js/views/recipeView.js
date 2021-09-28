4c1c7503b92d6c5da71ac27c5f9b1ffc39804888
Rafał Wojtkowiak <world@macbook-air-rafa.home>
2021-09-28 20:27:54 +0200

    removing quantity null bug

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -113,7 +113,7 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity}</div>
+        <div class="recipe__quantity">${ing.quantity ? ing.quantity : ''}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}

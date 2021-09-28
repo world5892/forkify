610bb91d11dc612bf626563b2bfd065181d9cd60
Rafał Wojtkowiak <world@macbook-air-rafa.home>
2021-09-28 20:24:00 +0200

    deleted fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js'
 import icons from 'url:../../img/icons.svg'
-const Fraction = require('fractional').Fraction
+// const Fraction = require('fractional').Fraction
 
 
 class RecipeView extends View {
@@ -113,7 +113,7 @@ class RecipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+        <div class="recipe__quantity">${ing.quantity}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}

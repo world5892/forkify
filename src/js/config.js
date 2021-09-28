6242a67cd17d06371ff4713932d19b6add16e8c4
Rafał Wojtkowiak <world@macbook-air-rafa.home>
2021-09-28 19:23:25 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = '28763b21-4096-4249-82e2-3b3c1399e153'
+export const MODAL_CLOSE_SEC = 2.5
